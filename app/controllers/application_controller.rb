f5f74df8cd4580b714b44a8450c352f16e060839
Kevin Briggs <kevinbriggs1@gmail.com>
2022-05-02 08:06:58 -0400

    styled login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+private
+
+  def require_login
+    unless current_member || request.path == "/members/sign_in"
+      redirect_to "/members/sign_in"
+    end
+  end
 end
