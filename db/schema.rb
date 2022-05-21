6c4a482c70efe6d952053f87b8c0ec66784d7cee
Kevin Briggs <kevinbriggs1@gmail.com>
2022-05-21 09:28:53 -0400

    rails 7 is running

--- db/schema.rb
+++ db/schema.rb
@@ -2,40 +2,39 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_020457) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_132455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "families", id: :integer, default: nil, force: :cascade do |t|
     t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "health_goals", id: :integer, default: nil, force: :cascade do |t|
     t.text "goal"
     t.bigint "status", default: 0
     t.bigint "goal_type", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "member_id"
     t.bigint "meeting_id"
   end
 
   create_table "meetings", id: :integer, default: nil, force: :cascade do |t|
     t.text "members"
-    t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "date", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "family_id"
   end
 
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 2022_03_22_020457) do
     t.text "email", default: "", null: false
     t.text "encrypted_password", default: "", null: false
     t.text "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.bigint "family_id"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
