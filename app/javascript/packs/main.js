0948d0629006d0608bf62dcc3cc64d460612a418
Kevin Briggs <kevinbriggs1@gmail.com>
2022-06-04 21:16:59 -0400

    combined to one function

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -14,32 +14,17 @@ function handleProfileCredits() {
   const member_use_credits = document.querySelectorAll('.member_use_credit')  
   if (!member_use_credits.length) return;
   const member_add_credits = document.querySelectorAll('.member_add_credit')  
+  updateCredits(member_add_credits, 'POST', '/add-credit')
+  updateCredits(member_use_credits, 'DELETE', '/remove-credit')
+}
 
-  for (let member_add_credit of member_add_credits) {
-    const body = { ...member_add_credit.dataset }
+function updateCredits(elems, method, url) {
+  for (let el of elems) {
+    const body = { ...el.dataset }
     const crsfToken = document.querySelector("[name='csrf-token']").getAttribute('content')
-    member_add_credit.addEventListener('click', ()=> {
-      fetch('/add-credit', {
-        method: 'POST',
-        headers: {
-          'X-CSRF-Token': crsfToken,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      })
-      .then(res => res.json())
-      .then(res => {
-        const el = document.getElementById(`member-credit-count-${body.receivingMemberId}`)
-        el.innerHTML = res.count
-      })
-    })
-  }
-  for (let member_use_credit of member_use_credits) {
-    const body = { ...member_use_credit.dataset }
-    const crsfToken = document.querySelector("[name='csrf-token']").getAttribute('content')
-    member_use_credit.addEventListener('click', ()=> {
-      fetch('/remove-credit', {
-        method: 'DELETE',
+    el.addEventListener('click', ()=> {
+      fetch(url, {
+        method: method,
         headers: {
           'X-CSRF-Token': crsfToken,
           'Content-Type': 'application/json'
